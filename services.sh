38ef8167d7a7c443bdd7525673bc379f98be6f6f
stu2005 <konstu8@yahoo.co.jp>
2022-01-28 01:19:34 +0900

    fix entrypoint

--- services.sh
+++ services.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-if [ -e "/etc/init.d/pcscd" ]; then
-  /etc/init.d/pcscd stop > /dev/null 2>&1 || echo "stoping pcscd..."
-  sleep 2
-  /etc/init.d/pcscd start > /dev/null 2>&1 || echo "starting pcscd..."
-fi
-
+/etc/init.d/pcscd start
 echo "Start mirakc"
 mirakc
